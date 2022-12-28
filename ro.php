58bf1bdcbb2143b99a9faa838eb4218a6eb5cbd8
motoraz <121549475+motoraz@users.noreply.github.com>
2022-12-28 11:19:34 +0100

    Add files via upload

---
+++ ro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+<?php
+include "components/DataBase.php";
+$lang = 1; // 0 = english; 1 = romanian
+?>
+
+<?php include 'components/indexTemplate.php' ?>
+
+</html>
